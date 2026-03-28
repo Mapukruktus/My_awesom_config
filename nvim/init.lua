651a731d96e3aad73b13dd982a443b8aae96f36b
Mapukruktus <thanachat26.p@gmail.com>
2026-03-29 00:15:25 +0700

    test lazygit

--- nvim/init.lua
+++ nvim/init.lua
@@ -112,8 +112,6 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
-      -- delay between pressing a key and opening which-key (milliseconds)
-      -- this setting is independent of vim.o.timeoutlen
       delay = 0,
       icons = {
         mappings = vim.g.have_nerd_font,
